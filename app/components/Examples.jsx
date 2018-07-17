6df43e8427c8d6aff89ef6ce3fed0709020600e4
abdul <abdul.pesit@gmail.com>
2018-07-17 09:18:52 +0530

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h3>Examples component stateless func news</h3>
+    <h3>Examples component stateless func news after git setup</h3>
   )
 };
 module.exports = Examples;
