7aafd6c6660bea51d676c7232b0be1de17ab2354
abdul <abdul.pesit@gmail.com>
2018-08-20 07:55:24 +0530

    Fix error modal bug

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -45,14 +45,14 @@ var Weather = React.createClass({
 
     }
   },
-  componentWillReceiveProps: function (newProps) {
-    var location = newProps.location.query.location;
-
-    if (location && location.length > 0) {
-      this.handleSearch(location);
-      window.location.hash = '#/?location='+location;
-    }
-  },
+  // componentWillReceiveProps: function (newProps) {
+  //   var location = newProps.location.query.location;
+  //
+  //   if (location && location.length > 0) {
+  //     this.handleSearch(location);
+  //     window.location.hash = '#/?location='+location;
+  //   }
+  // },
 
   render: function(){
     var {isLoading,temp, location, errorMessage} = this.state;
